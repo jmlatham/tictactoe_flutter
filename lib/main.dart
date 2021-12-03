e302261d6a397dc92bdbc20eaf5312272cfde1c1
jmlatham <jmlatham@yahoo.com>
2021-12-02 20:13:47 -0600

    Added the buttons. Added the onPressed actions. Currently has enough logic for two people to play one game.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tic-Tac-Toe with Flutter',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Tic-Tac-Toe'),
     );
   }
 }
@@ -48,7 +50,43 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _player = 0;
+  int _computer = 1;
+  int _cat = -1;
   int _counter = 0;
+  int _winner = -1;
+  int _turnCounter = -1;
+  bool _isPlayerTurn = false;
+  bool _gameIsOver = false;
+  bool _gameHasStarted = false;
+  bool _topEnabled = true;
+  bool _rightEnabled = true;
+  bool _bottomEnabled = true;
+  bool _leftEnabled = true;
+  bool _topLeftEnabled = true;
+  bool _topRightEnabled = true;
+  bool _bottomLeftEnabled = true;
+  bool _bottomRightEnabled = true;
+  bool _centerEnabled = true;
+  IconData _computerIcon = Icons.close_outlined;
+  IconData _playerIcon = Icons.circle_outlined;
+  IconData _defaultIcon = Icons.crop_3_2_outlined;
+  // int _row = 3;
+  // int _col = 4;
+  // var twoDList = List.generate(3, (i) => List.generate(3, (j) => j, growable:false), growable: false);
+  late List<List<IconData>> _values; //= List<List<IconData>>.generate(3, (_) => List<IconData>.filled(3, _defaultIcon, growable: false));
+
+  _MyHomePageState(){
+    _values = List<List<IconData>>.generate(3, (_) => List<IconData>.filled(3, _defaultIcon, growable: false));
+    // for(var i = 0; i < _values.length; i++){
+    //   for(var j = 0; i < _values[i].length; i++){
+    //     // print(_values[i][j]);
+    //     _values[i][j] = _defaultIcon;
+    //   }
+    // }
+    Random random = Random();
+    _isPlayerTurn = random.nextInt(2) == 1;
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -61,8 +99,53 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  bool getGameIsOver(){
+    if(_turnCounter > 7){
+      _winner = _cat;
+      return true;
+    }
+    // for(var i = 0; i < 3; i++){
+    //   if (_values[i].contains(_defaultIcon)) return false;
+    // }
+    _winner = (_isPlayerTurn)? _player : _computer;
+    for(var i = 0; i < 3; i++){
+      if (rowIsEqual(i)) return true;
+    }
+    for(var i = 0; i < 3; i++){
+      if (colIsEqual(i)) return true;
+    }
+    return diagonalIsEqual();
+  }
+
+  bool rowIsEqual(int row){
+    return !_values[row].contains(_defaultIcon) && [row][0] == _values[row][1] &&
+    _values[row][0] == _values[row][2];
+  }
+
+  bool colIsEqual(int col){
+    if(_values[0][col] == _defaultIcon){
+      return false;
+    }
+    return _values[0][col] == _values[1][col] &&
+        _values[0][col] == _values[2][col];
+  }
+
+  bool diagonalIsEqual(){
+    if(_values[0][0] == _defaultIcon || _values[0][2] == _defaultIcon){
+      return false;
+    }
+    return (
+      _values[0][0] == _values[1][1] &&
+        _values[1][1] == _values[2][2]
+    ) || (
+        _values[0][2] == _values[1][1] &&
+            _values[1][1] == _values[2][0]
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    // final gameHasStarted = _gameHasStarted[0];
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -94,22 +177,223 @@ class _MyHomePageState extends State<MyHomePage> {
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children:[
+            Row(
+              children:[
+                IconButton(
+                  icon: Icon(
+                      // (_gameHasStarted && !_topLeftEnabled)?
+                          _values[0][0]
+                      // (_isPlayerTurn)?
+                      // _playerIcon:_computerIcon
+                      //     :_defaultIcon
+                  ),
+                  onPressed: _topLeftEnabled && !_gameIsOver ?() {
+                    setState(() {
+                      _topLeftEnabled = false;
+                      _values[0][0] = (_isPlayerTurn)?_playerIcon:_computerIcon;
+                      _gameHasStarted = true;
+                      _gameIsOver = getGameIsOver();
+                      if(_gameIsOver){
+                        displayGameOverMessage();
+                      } else {
+                        _isPlayerTurn = !_isPlayerTurn;
+                      }
+                    });
+                  }:null,
+                ),
+                IconButton(
+                  icon: Icon(
+                      // (_gameHasStarted && !_topEnabled)?
+                          _values[0][1]
+                      // (_isPlayerTurn)?
+                      // _playerIcon:_computerIcon
+                      //     :_defaultIcon
+                  ),
+                  onPressed: _topEnabled && !_gameIsOver ?() {
+                    setState(() {
+                      _topEnabled = false;
+                      _values[0][1] = (_isPlayerTurn)?_playerIcon:_computerIcon;
+                      _gameIsOver = getGameIsOver();
+                      if(_gameIsOver){
+                        displayGameOverMessage();
+                      } else {
+                        _isPlayerTurn = !_isPlayerTurn;
+                      }
+                    });
+                  }:null,
+                ),
+                IconButton(
+                  icon: Icon(
+                    _values[0][2]
+                      // (_gameHasStarted)?
+                      // (_isPlayerTurn)?
+                      // _playerIcon:_computerIcon:_defaultIcon
+                  ),
+                  onPressed: _topRightEnabled && !_gameIsOver ?() {
+                    setState(() {
+                      _topRightEnabled = false;
+                      _values[0][2] = (_isPlayerTurn)?_playerIcon:_computerIcon;
+                      _gameIsOver = getGameIsOver();
+                      if(_gameIsOver){
+                        displayGameOverMessage();
+                      } else {
+                        _isPlayerTurn = !_isPlayerTurn;
+                      }
+                    });
+                  }:null,
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Row(
+              children:[
+                IconButton(
+                  icon: Icon(
+                    _values[1][0]
+                      // (_gameHasStarted)?
+                      // (_isPlayerTurn)?
+                      // _playerIcon:_computerIcon:_defaultIcon
+                  ),
+                  onPressed: _leftEnabled && !_gameIsOver ?() {
+                    setState(() {
+                      _leftEnabled = false;
+                      _values[1][0] = (_isPlayerTurn)?_playerIcon:_computerIcon;
+                      _gameIsOver = getGameIsOver();
+                      if(_gameIsOver){
+                        displayGameOverMessage();
+                      } else {
+                        _isPlayerTurn = !_isPlayerTurn;
+                      }
+                    });
+                  }:null,
+                ),
+                IconButton(
+                  icon: Icon(
+                    _values[1][1]
+                      // (_gameHasStarted)?
+                      // (_isPlayerTurn)?
+                      // _playerIcon:_computerIcon:_defaultIcon
+                  ),
+                  onPressed: _centerEnabled && !_gameIsOver ?() {
+                    setState(() {
+                      _centerEnabled = false;
+                      _values[1][1] = (_isPlayerTurn)?_playerIcon:_computerIcon;
+                      _gameIsOver = getGameIsOver();
+                      if(_gameIsOver){
+                        displayGameOverMessage();
+                      } else {
+                        _isPlayerTurn = !_isPlayerTurn;
+                      }
+                    });
+                  }:null,
+                ),
+                IconButton(
+                  icon: Icon(
+                    _values[1][2]
+                      // (_gameHasStarted)?
+                      // (_isPlayerTurn)?
+                      // _playerIcon:_computerIcon:_defaultIcon
+                  ),
+                  onPressed: _rightEnabled && !_gameIsOver ?() {
+                    setState(() {
+                      _rightEnabled = false;
+                      _values[1][2] = (_isPlayerTurn) ? _playerIcon : _computerIcon;
+                      _gameIsOver = getGameIsOver();
+                      if (_gameIsOver) {
+                        displayGameOverMessage();
+                      } else {
+                        _isPlayerTurn = !_isPlayerTurn;
+                      }
+                    });
+                  }:null,
+                ),
+              ],
+            ),
+            Row(
+              children:[
+                IconButton(
+                  icon: Icon(
+                    _values[2][0]
+                      // (_gameHasStarted)?
+                      // (_isPlayerTurn)?
+                      // _playerIcon:_computerIcon:_defaultIcon
+                  ),
+                  onPressed: _bottomLeftEnabled && !_gameIsOver ?() {
+                    setState(() {
+                      _bottomLeftEnabled = false;
+                      _values[2][0] = (_isPlayerTurn)?_playerIcon:_computerIcon;
+                      _gameIsOver = getGameIsOver();
+                      if (_gameIsOver) {
+                        displayGameOverMessage();
+                      } else {
+                        _isPlayerTurn = !_isPlayerTurn;
+                      }
+                    });
+                  }:null,
+                ),
+                IconButton(
+                  icon: Icon(
+                    _values[2][1]
+                      // (_gameHasStarted)?
+                      // (_isPlayerTurn)?
+                      // _playerIcon:_computerIcon:_defaultIcon
+                  ),
+                  onPressed: _bottomEnabled && !_gameIsOver ?() {
+                    setState(() {
+                      _bottomEnabled = false;
+                      _values[2][1] = (_isPlayerTurn)?_playerIcon:_computerIcon;
+                      _gameIsOver = getGameIsOver();
+                      if (_gameIsOver) {
+                        displayGameOverMessage();
+                      } else {
+                        _isPlayerTurn = !_isPlayerTurn;
+                      }
+                    });
+                  }:null,
+                ),
+                IconButton(
+                  icon: Icon(
+                    _values[2][2]
+                      // (_gameHasStarted)?
+                      // (_isPlayerTurn)?
+                      // _playerIcon:_computerIcon:_defaultIcon
+                  ),
+                  onPressed: _bottomRightEnabled && !_gameIsOver ?() {
+                    setState(() {
+                      _bottomRightEnabled = false;
+                      _values[2][2] = (_isPlayerTurn)?_playerIcon:_computerIcon;
+                      _gameIsOver = getGameIsOver();
+                      if (_gameIsOver) {
+                        displayGameOverMessage();
+                      } else {
+                        _isPlayerTurn = !_isPlayerTurn;
+                      }
+                    });
+                  }:null,
+                ),
+              ],
             ),
           ],
+          // children: <Widget>[
+          //   const Text(
+          //     'You have pushed the button this many times:',
+          //   ),
+          //   Text(
+          //     '$_counter',
+          //     style: Theme.of(context).textTheme.headline4,
+          //   ),
+          // ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void displayGameOverMessage(){
+
   }
 }
